5a143f7381010785de44d5236025fa94ed94d762
Dogan Ulus <doganulus@gmail.com>
2022-06-06 23:31:01 +0300

    Use Bootstrap module
    Signed-off-by: Dogan Ulus <doganulus@gmail.com>

---
+++ assets/js/index.js
@@ -0,0 +1,4 @@
+// Import the Bootstrap components we want to use.
+// See https://github.com/twbs/bootstrap/blob/main/js/index.umd.js
+//  import Carousel from "js/bootstrap/src/carousel";
+//  import Tab from "js/bootstrap/src/tab";
