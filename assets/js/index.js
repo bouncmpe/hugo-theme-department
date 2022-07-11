1d687b238654bb7144c125f0b9d5e6ac1dd04683
Dogan Ulus <doganulus@gmail.com>
2022-07-11 17:38:01 +0300

    Fix bootstrap setup and customize default colors
    Signed-off-by: Dogan Ulus <doganulus@gmail.com>

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,17 @@
 // Import the Bootstrap components we want to use.
 // See https://github.com/twbs/bootstrap/blob/main/js/index.umd.js
-//  import Carousel from "js/bootstrap/src/carousel";
-//  import Tab from "js/bootstrap/src/tab";
+// import Carousel from "js/bootstrap/src/carousel";
+// import Tab from "js/bootstrap/src/tab";
+
+import Alert from 'js/bootstrap/src/alert'
+import Button from 'js/bootstrap/src/button'
+import Carousel from 'js/bootstrap/src/carousel'
+import Collapse from 'js/bootstrap/src/collapse'
+import Dropdown from 'js/bootstrap/src/dropdown'
+import Modal from 'js/bootstrap/src/modal'
+import Offcanvas from 'js/bootstrap/src/offcanvas'
+import Popover from 'js/bootstrap/src/popover'
+import ScrollSpy from 'js/bootstrap/src/scrollspy'
+import Tab from 'js/bootstrap/src/tab'
+import Toast from 'js/bootstrap/src/toast'
+import Tooltip from 'js/bootstrap/src/tooltip'
